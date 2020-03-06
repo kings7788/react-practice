1ebcf475986c5a7d5048dee04f7c69194d0f69d1
bryant <bryant.chang@cloud-interactive.com>
2020-03-06 14:47:45 +0800

    Updates

---
+++ precache-manifest.47e996506acc946460092be0c62b3a19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284ff5403097591744d687de46017989",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "29dab76c373ccc9e3be3",
+    "url": "/react-practice/static/css/main.3e7cfe59.chunk.css"
+  },
+  {
+    "revision": "3677a31b80c26b390723",
+    "url": "/react-practice/static/js/2.14b07f12.chunk.js"
+  },
+  {
+    "revision": "29dab76c373ccc9e3be3",
+    "url": "/react-practice/static/js/main.b6cf8089.chunk.js"
+  },
+  {
+    "revision": "7ec0bb9ac3a2f402aa93",
+    "url": "/react-practice/static/js/runtime-main.0aeebbaf.js"
+  }
+]);
